ae98435a248975f419f0528daf0081d2832bd3db
Alexandre Melo <xandymelo@outlook.com.br>
2020-01-28 09:30:29 -0300

    em processo

--- PlayChess.py
+++ PlayChess.py
@@ -13,8 +13,50 @@ chessBoard = Board()
 chessBoard.createBoard()
 chessBoard.printBoard()
 
+allTiles = []
+allPieces = []
 
 
+def squares(x,y,w,h,color):
+    pygame.draw.rect(gameDisplay, color, [x,y,w,h]) #desenhe um retângulo#
+    allTiles.append([color,[x,y,w,h]]) 
+
+def drawChessPieces():
+    xpos = 0
+    ypos = 0
+    color = 0
+    width = 100
+    height = 100
+    black = (66,134,244)
+    white = (143,155,175)
+    number = 0
+    for _ in range(8):
+        for _ in range(8):
+            if color % 2 == 0:
+                squares(xpos, ypos, width, height, white)
+                if not chessBoard.gameTiles[number].pieceOnTile.toString() == "-":
+                    img = pygame.image.load("./ChessArt/" + chessBoard.gameTiles[number].pieceOnTile.alliance[0].upper() + chessBoard.gameTiles[
+                        number].pieceOnTile.toString().upper() + ".png")
+                    img = pygame.transform.scale(img, (100, 100))
+                    allPieces.append([img, [xpos, ypos], chessBoard.gameTiles[number].pieceOnTile])
+                xpos += 100
+            else:
+                squares(xpos, ypos, width, height, black)
+                if not chessBoard.gameTiles[number].pieceOnTile.toString() == "-":
+                    img = pygame.image.load("./ChessArt/" + chessBoard.gameTiles[number].pieceOnTile.alliance[0].upper() + chessBoard.gameTiles[
+                        number].pieceOnTile.toString().upper() + ".png")
+                    img = pygame.transform.scale(img, (100, 100))
+                    allPieces.append([img, [xpos, ypos], chessBoard.gameTiles[number].pieceOnTile])
+                xpos += 100
+
+            color += 1
+            number += 1
+        color += 1
+        xpos = 0
+        ypos += 100
+
+
+drawChessPieces()
 
 quitGame = False
 
@@ -26,6 +68,10 @@ while not quitGame:
             quitGame = True
             pygame.quit()
             quit()
+    
+
+    for img in allPieces:
+        gameDisplay.blit(img[0],img[1]) #desenhar uma imagem na outra#
 
     pygame.display.update() #Atualizar partes da tela para exibições de software#
     clock.tick(60) #atualizar o relógio#
