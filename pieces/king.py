18ea2106597f73eb26a5f34dc9128398c11c438a
Alexandre Melo <xandymelo@outlook.com.br>
2020-02-12 07:54:56 -0300

    processo

--- pieces/king.py
+++ pieces/king.py
@@ -26,22 +26,22 @@ class King(Piece):
             mov_possiveis.remove(self.position - 8)
             mov_possiveis.remove(self.position - 7)
             mov_possiveis.remove(self.position - 9)
-        elif self.position in self.ultima_linha:
+        if self.position in self.ultima_linha:
             mov_possiveis.remove(self.position + 7)
             mov_possiveis.remove(self.position + 8)
             mov_possiveis.remove(self.position + 9)
-        elif self.position in self.primeira_coluna:
+        if self.position in self.primeira_coluna:
             mov_possiveis.remove(self.position - 1)
-        if self.position + 7 in mov_possiveis:
-            mov_possiveis.remove(self.position + 7)
-        if self.position - 9 in mov_possiveis:
-            mov_possiveis.remove(self.position - 9)
-        elif self.position in self.ultima_coluna:
+            if self.position + 7 in mov_possiveis:
+                mov_possiveis.remove(self.position + 7)
+            if self.position - 9 in mov_possiveis:
+                mov_possiveis.remove(self.position - 9)
+        if self.position in self.ultima_coluna:
+            mov_possiveis.remove(self.position + 1)
             if self.position + 9 in mov_possiveis:
                 mov_possiveis.remove(self.position + 9)
-            mov_possiveis.remove(self.position + 1)
-        if self.position - 7 in mov_possiveis:
-            mov_possiveis.remove(self.position - 7)
+            if self.position - 7 in mov_possiveis:
+                mov_possiveis.remove(self.position - 7)
 
         if (cor_do_jogador == "BLACK"):
             for c in mov_possiveis:
--- pieces/piece.py
+++ pieces/piece.py
@@ -62,17 +62,17 @@ class Piece():
 
     def todos_os_movimentos_possiveis(self,cor_do_jogador):#Note que não adicionei os movimentos dos reis, pois vou usar esta função para verificar xeque, e xeque mate#
         mov_possiveis = []
-        if cor_do_jogador == "PRETAS" or cor_do_jogador == "NEGRAS":
+        if cor_do_jogador == "BLACK":
             #mov_possiveis = torre(self.gameTiles.index("Torre2Pretas"),cor_do_jogador).mov_possiveis(self.gameTiles) + torre(self.gameTiles.index("TorrePretas"),cor_do_jogador).mov_possiveis(self.gameTiles) + bispo.bispo(self.gameTiles.index("BispoPPretas"),cor_do_jogador).mov_possiveis(self.gameTiles) + bispo.bispo(self.gameTiles.index("BispoBPretas"),cor_do_jogador).mov_possiveis(self.gameTiles) + cavalo(self.gameTiles.index("CavaloPretas"),cor_do_jogador).mov_possiveis(self.gameTiles) + cavalo(self.gameTiles.index("Cavalo2Pretas"),cor_do_jogador).mov_possiveis(self.gameTiles) + dama(self.gameTiles.index("DamaPretas"),cor_do_jogador).mov_possiveis(self.gameTiles.index("DamaPretas")) + peao(self.gameTiles.index("peãoPretas1"),cor_do_jogador).mov_possiveis(self.gameTiles.index("peãoPretas1")) + peao(self.gameTiles.index("peãoPretas2"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoPretas3"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoPretas4"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoPretas5"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoPretas6"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoPretas7"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoPretas8"),cor_do_jogador).mov_possiveis(self.gameTiles)
             for c in self.gameTiles:
-                if c.toString().isupper():
-                    for d in c.possible_mov():
+                if c.pieceOnTile.toString().isupper():
+                    for d in c.pieceOnTile.possible_mov():
                         if d not in mov_possiveis:
                             mov_possiveis.append(d)
-        if cor_do_jogador == "BRANCAS":
+        if cor_do_jogador == "WHITE":
             for c in self.gameTiles:
-                if c.toString().islower():
-                    for d in c.possible_mov():
+                if c.pieceOnTile.toString().islower():
+                    for d in c.pieceOnTile.possible_mov():
                         if d not in mov_possiveis:
                             mov_possiveis.append(d)
             #mov_possiveis = torre(self.gameTiles.index("Torre2Brancas"),cor_do_jogador).mov_possiveis(self.gameTiles) + torre(self.gameTiles.index("TorreBrancas"),cor_do_jogador).mov_possiveis(self.gameTiles) + bispo.bispo(self.gameTiles.index("BispoPBrancas"),cor_do_jogador).mov_possiveis(self.gameTiles) + bispo.bispo(self.gameTiles.index("BispoBBrancas"),cor_do_jogador).mov_possiveis(self.gameTiles) + cavalo(self.gameTiles.index("CavaloBrancas"),cor_do_jogador).mov_possiveis(self.gameTiles) + cavalo(self.gameTiles.index("Cavalo2Brancas"),cor_do_jogador).mov_possiveis(self.gameTiles) + dama(self.gameTiles.index("DamaBrancas"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoBrancas1"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoBrancas2"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoBrancas3"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoBrancas4"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoBrancas5"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoBrancas6"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoBrancas7"),cor_do_jogador).mov_possiveis(self.gameTiles) + peao(self.gameTiles.index("peãoBrancas8"),cor_do_jogador).mov_possiveis(self.gameTiles)
